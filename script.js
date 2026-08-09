99a19d393c0c45c5df955925b453d89f5592ab31
mqlam <mqlamcreate@gmail.com>
2026-08-09 14:25:38 -0500

    2

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.getElementById("helloBtn").addEventListener("click", () => {
     alert("Hello, World!");
 });
+
+let dots = 1;
+
+setInterval(() => {
+    dots++;
+
+    if (dots > 3) {
+        dots = 1;
+    }
+
+    document.getElementById("loadingText").textContent =
+        "Loading" + ".".repeat(dots);
+
+}, 500);
